a6e95d914e32c732d35abf893c06e4094af39e76
KannanGOfficial <kannang143592@gmail.com>
2024-11-08 10:06:54 +0530

    Ui Bug Fixes

--- app/src/main/java/com/nifa/fuel_buddy/presentation/feature/home/OrderStatusScreen.kt
+++ app/src/main/java/com/nifa/fuel_buddy/presentation/feature/home/OrderStatusScreen.kt
@@ -1,22 +1,30 @@
 package com.nifa.fuel_buddy.presentation.feature.home
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun OrderStatusScreen(modifier: Modifier = Modifier) {
-    Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("OrderStatusScreen")
+fun OrderStatusScreen(
+    modifier: Modifier = Modifier,
+    uiState: OrderStatusScreenUiState
+) {
+
+    when (uiState.screenState) {
+        OrderStatusScreenState.LOADING -> {
+            LoaderScreenContent(modifier)
+        }
+
+        OrderStatusScreenState.APPROVED -> {
+            OrderApprovedScreenContent(modifier)
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 private fun OrderStatusScreenPreview() {
-    OrderStatusScreen()
+    OrderStatusScreen(
+        uiState = OrderStatusScreenUiState()
+    )
 }
