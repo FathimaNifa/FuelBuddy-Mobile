402b61d0997fc8e8e7437adeccd980f50d47af90
KannanGOfficial <kannang143592@gmail.com>
2024-11-14 21:18:47 +0530

    Finishes SignUp Auth Ui

---
+++ app/src/main/java/com/nifa/fuel_buddy/fuelstation/presentation/DLVerificationScreen.kt
@@ -0,0 +1,16 @@
+package com.nifa.fuel_buddy.fuelstation.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun DLVerificationScreen(modifier: Modifier = Modifier) {
+
+}
+
+@Preview
+@Composable
+private fun DLVerificationScreenPreview() {
+    DLVerificationScreen()
+}
--- app/src/main/java/com/nifa/fuel_buddy/fuelstation/presentation/navigation/AuthNavGraph.kt
+++ app/src/main/java/com/nifa/fuel_buddy/fuelstation/presentation/navigation/AuthNavGraph.kt
@@ -4,11 +4,16 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
+import com.nifa.fuel_buddy.fuelstation.presentation.ChooseSignUpScreen
+import com.nifa.fuel_buddy.fuelstation.presentation.DLVerificationScreen
+import com.nifa.fuel_buddy.fuelstation.presentation.ForgotPasswordScreen
+import com.nifa.fuel_buddy.fuelstation.presentation.FuelStationSignUpScreen
 import com.nifa.fuel_buddy.fuelstation.presentation.SignInScreen
+import com.nifa.fuel_buddy.fuelstation.presentation.UserSignUpScreen
 
 fun NavGraphBuilder.authNavGraph(navController: NavController) {
     navigation<AuthNavigation.AuthNavGraph>(
-        startDestination = AuthNavigation.SignInScreen
+        startDestination = AuthNavigation.UserSignUpScreen
     ) {
 
         composable<AuthNavigation.SignInScreen> {
@@ -16,23 +21,23 @@ fun NavGraphBuilder.authNavGraph(navController: NavController) {
         }
 
         composable<AuthNavigation.ChooseSignUpScreen> {
-
+            ChooseSignUpScreen()
         }
 
         composable<AuthNavigation.UserSignUpScreen> {
-
+            UserSignUpScreen()
         }
 
         composable<AuthNavigation.FuelStationSignUpScreen> {
-
+            FuelStationSignUpScreen()
         }
 
         composable<AuthNavigation.DLVerificationScreen> {
-
+            DLVerificationScreen()
         }
 
         composable<AuthNavigation.ForgotPasswordScreen> {
-
+            ForgotPasswordScreen()
         }
     }
 }
