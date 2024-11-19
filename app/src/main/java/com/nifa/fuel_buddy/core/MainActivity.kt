5eac4807f81ca59539d2a84760d1953cda601bd2
KannanGOfficial <kannang143592@gmail.com>
2024-11-19 07:43:51 +0530

    Finishes Start Destination Logic based on Preferences

--- app/src/main/java/com/nifa/fuel_buddy/core/MainActivity.kt
+++ app/src/main/java/com/nifa/fuel_buddy/core/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.getValue
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.rememberNavController
-import com.nifa.fuel_buddy.core.navigation.NavGraphs
 import com.nifa.fuel_buddy.core.navigation.SetupMainNavGraph
 import com.nifa.fuel_buddy.core.ui.theme.FuelBuddyMobileTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,14 +18,21 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val splashScreen = installSplashScreen()
         setContent {
             FuelBuddyMobileTheme {
 
+                val viewModel = hiltViewModel<MainViewModel>()
+                val uiState by viewModel.uiState.collectAsStateWithLifecycle()
                 val navHostController = rememberNavController()
+
+                splashScreen.setKeepOnScreenCondition {
+                    uiState.keepSplashScreen
+                }
 
                 SetupMainNavGraph(
                     navHostController = navHostController,
-                    startDestination = NavGraphs.AuthNavGraph
+                    startDestination = uiState.startDestination
                 )
             }
         }
