48e16113fa467d7277b41c612127f5fe13da11b2
KannanGOfficial <kannang143592@gmail.com>
2024-12-09 20:12:34 +0530

    Finishes Update Driver Location Data layer setup

--- app/src/main/java/com/nifa/fuel_buddy/fuelstation/data/networkSource/FuelStationNetworkSource.kt
+++ app/src/main/java/com/nifa/fuel_buddy/fuelstation/data/networkSource/FuelStationNetworkSource.kt
@@ -2,12 +2,14 @@ package com.nifa.fuel_buddy.fuelstation.data.networkSource
 
 import com.nifa.fuel_buddy.core.domain.NetworkError
 import com.nifa.fuel_buddy.core.domain.Result
+import com.nifa.fuel_buddy.core.domain.SuccessResponse
 import com.nifa.fuel_buddy.fuelstation.data.model.GetCustomerOrderDto
 import com.nifa.fuel_buddy.fuelstation.data.model.GetOrderedProductDetailsDto
 import com.nifa.fuel_buddy.fuelstation.domain.model.request.GetCustomerOrdersRequest
 import com.nifa.fuel_buddy.fuelstation.domain.model.request.GetOrderHistoryRequest
 import com.nifa.fuel_buddy.fuelstation.domain.model.request.GetOrderedHistoryProductsRequest
 import com.nifa.fuel_buddy.fuelstation.domain.model.request.GetOrderedProductsRequest
+import com.nifa.fuel_buddy.fuelstation.domain.model.request.UpdateDriverLocationRequest
 import kotlinx.coroutines.flow.Flow
 
 interface FuelStationNetworkSource {
@@ -19,4 +21,6 @@ interface FuelStationNetworkSource {
     suspend fun getOrderHistory(request: GetOrderHistoryRequest): Flow<Result<GetCustomerOrderDto, NetworkError>>
 
     suspend fun getOrderHistoryProducts(request: GetOrderedHistoryProductsRequest): Flow<Result<GetOrderedProductDetailsDto, NetworkError>>
+
+    suspend fun updateDriverLocation(request: UpdateDriverLocationRequest): Flow<Result<SuccessResponse, NetworkError>>
 }
---
+++ app/src/main/java/com/nifa/fuel_buddy/fuelstation/domain/model/request/UpdateDriverLocationRequest.kt
@@ -0,0 +1,6 @@
+package com.nifa.fuel_buddy.fuelstation.domain.model.request
+
+data class UpdateDriverLocationRequest(
+    val latitude : Double,
+    val longitude : Double
+)
